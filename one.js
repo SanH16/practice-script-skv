1c025ea677cf15f1e1387c8ac46b1f128c96b61a
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-09-10 16:15:02 +0700

    update learn JS

---
+++ one.js
@@ -0,0 +1,85 @@
+// console.log("Halo boy")
+
+var hitung = 1;
+let nama = "Adhitya";
+const namaBelakang = "Hasan";
+
+console.log(hitung);
+console.log(nama);
+console.log(namaBelakang);
+
+// ====================================
+let namaa = "Stefanus";
+const namaIbuKandung = "Clara";
+const umur = 20;
+const apakahSudahMenikah = false;
+let jumlahAnak;
+
+console.log(namaa);
+console.log(namaIbuKandung);
+console.log(umur);
+console.log(apakahSudahMenikah);
+console.log(jumlahAnak);
+
+// ============== Tutor Dea Afrizal ======================
+console.log(nama);
+let usia = 20;
+let tinggiBadan = 165;
+let beratBadan;
+let pacar = 2;
+
+beratBadan = 59;
+
+if (pacar == null) {
+  pacar = "Belum punya";
+} else {
+  pacar = "Sudah punya";
+}
+
+// atau
+// switch(pacar){
+//     case 1:
+//         pacar = "punya 1";
+//         break
+//     case 2:
+//         pacar = "punya 2, aku cukup playboy";
+//         break
+//     default:
+//         pacar = "belum punya weh";
+//         break
+// }
+
+let siap = '"Jika yakin masuk kesini tekan tombol OK"';
+let saldoAwal = 80000;
+let saldoTambahan = 50000;
+let hutang = 20000;
+const saldoAkhir = saldoAwal + saldoTambahan - hutang;
+
+alert(
+  `Haloo selamat datang ${nama} 🚀
+saat ini usia Anda adalah ${usia}
+tinggi badan Anda ${tinggiBadan} cm dan berat badan ${beratBadan} kg
+status pacar "${pacar}"❤️`
+);
+alert(`Halo ${nama} mohon dibaca dengan seksama ${siap}`);
+alert(
+  `Saat ini Saldo Awal anda sebesar Rp.${saldoAwal} 
+Saldo Tambahan sebesar Rp.${saldoTambahan}🧸
+Hutang yang belum dibayar Rp.${hutang}💳
+Maka total Saldo anda sebesar Rp.${saldoAkhir}💸`
+);
+
+// ============== Tutor Dea Afrizal ======================
+
+//======== new javascript refreshment =======
+//JS Refreshment
+
+let num = 3;
+
+if (num > 0) {
+  console.log(`${num} is a positive number`);
+} else {
+  console.log(`${num} is a negative number`);
+}
+
+console.log(num);
