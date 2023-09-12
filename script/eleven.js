e8df8ecadb9e18af03e8e92685e913a1fbab4068
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-09-12 23:14:02 +0700

    update finish basic learn JS

--- script/eleven.js
+++ script/eleven.js
@@ -26,20 +26,32 @@ sayHello("Adhitya", "Hasan"); // call Object with argument
 let person = {
   myFirstName: "Adhitya",
   myLastName: "Hasan",
-  sayHi: function () {
+  sayHi: () => {
     console.log(`Eyowezup ${this.myFirstName} ${this.myLastName}, How are u ?`);
   },
 };
-person.sayHi(); // call method in Object
+person.sayHi(); // call method/fungsi in Object
 
 // example 3
+
+let operasiMatematika = {
+  nama: "Penjumlahan",
+  operasi(angka1, angka2) {
+    let hasil = angka1 + angka2;
+    return `${this.nama} dari ${angka1} dan ${angka2} adalah ${hasil}`;
+  },
+};
+
+console.log(operasiMatematika.operasi(3, 5)); // output 8
+
+// example 4
 
 let mahasiswa = (name, npm, hobby) => {
   let anonymous = {
     name: name,
     npm: npm,
     hobbies: [hobby],
-    desc: function () {
+    desc: () => {
       console.log(`Eyow ${anonymous.name}, ur npm ${anonymous.npm}, ur hobby ${anonymous.hobbies}`);
     },
   };
@@ -47,7 +59,7 @@ let mahasiswa = (name, npm, hobby) => {
 };
 mahasiswa("adhitya Hasan", "2024240088", ["Ngoding", "tidur", "makan"].join(" - "));
 
-// example 4
+// example 5
 
 const product = (productName, productCategory, productPrice) => {
   let product = {
