a80fe5e2ad2a06554b8de585fc8ca2cd94fdcd6d
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-10-15 23:05:15 +0700

    update learn regex match

--- intermediate-2/three.js
+++ intermediate-2/three.js
@@ -1,1 +1,16 @@
 // ============================ Javasciprt Regex | Match and Flags ============================
+
+/**
+ * .match() sama seperti .exec() yaitu sebuah method bawaan (built-in) dari JavaScript.
+ * Namun .match() jika disisipkan suatu flag akan menghasilkan hasil yang beda dengan .exec().
+ 
+ * Contoh penggunaan .match():
+ */
+
+// ! contoh
+const regex = /i/;
+const words = "Indonesian";
+const result = regex.exec(words);
+
+console.log(words.match(regex)); // index : 7
+console.log(result); // index : 7
