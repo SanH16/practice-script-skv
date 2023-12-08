54ec38d945400a399d180f3ab13b9fc9549110cb
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-12-08 17:30:56 +0700

    complete Javasciprt Regex | Assesrtions

--- intermediate-2/four.js
+++ intermediate-2/four.js
@@ -39,3 +39,32 @@ console.log("============================= Special 'slash b' ===================
 const regexb = /ee\b/gi;
 const wordb = "Tree";
 console.log(wordb.match(regexb)); // Output: ['ee']
+
+// Contoh lain penggunaan karakter spesial \b:
+const regexb2 = /oo\b/gi;
+const wordb2 = "Moon";
+console.log(wordb2.match(regexb2)); // Output: null karena masih memiliki kata 'n' setelah huruf oo dalam kalimat "Moon"
+
+console.log("============================= x(?=y) lookahead =============================");
+
+// Contoh penggunaan lookahead, (lookahead): Hasilnya akan berupa 'x' hanya jika setelah 'x' diikuti oleh 'y' tanpa spasi.
+const lookahead = /Moon(?=Child)/;
+const lookaheadWord = "MoonChild";
+console.log(lookaheadWord.match(lookahead)); // Output : ["Moon"]
+
+// Contoh fail lookahead
+const lookahead2 = /Moon(?=Child)/;
+const lookaheadWord2 = "Moon Child";
+console.log(lookaheadWord2.match(lookahead2)); // output : null
+
+console.log("============================= (?<=y)x look behind =============================");
+
+// Contoh penggunaan lookbehind, (lookbehind): Hasilnya akan berupa 'x' hanya jika sebelum 'x' adalah 'y' tanpa spasi.
+const lookbehind = /(?<=Jekyll)Hyde/;
+const lookbehindWord = "JekyllHyde";
+console.log(lookbehindWord.match(lookbehind)); // Output : ["Hyde"]
+
+// Contoh fail lookbehind
+const lookbehind2 = /(?<=Jekyll)Hyde/;
+const lookbehindWord2 = "HydeJekyll";
+console.log(lookaheadWord2.match(lookbehind2)); // Output : null
